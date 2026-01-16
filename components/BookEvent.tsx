9772baac2099b940991b056e47e1519ca915f0ad
dskkoji <50835363+dskkoji@users.noreply.github.com>
2026-01-17 08:12:26 +0900

    finalize the app

--- components/BookEvent.tsx
+++ components/BookEvent.tsx
@@ -1,28 +1,34 @@
 'use client'
-import React from 'react'
-import { useState } from 'react'
 
-const BookEvent = ({ eventId, slug }: { eventId: string ; slug: string }) => {
+import { useState } from 'react'
+import { createBooking } from '@/lib/actions/booking.actions'
+import posthog from 'posthog-js'
+
+const BookEvent = ({ eventId, slug }: { eventId: string; slug: string; }) => {
   const [email, setEmail] = useState('')
   const [submitted, setSubmitted] = useState(false)
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
-    setTimeout(() => {
+
+    const { success } = await createBooking({ eventId, slug, email })
+    if (success) {
       setSubmitted(true)
-    }, 1000)
+      posthog.capture('event_booked', { eventId, slug, email })
+    } else {
+      console.error('Booking creation failed')
+      posthog.captureException('Booking creation failed')
+    }
   }
 
   return (
     <div id="book-event">
-      {submitted ?  (
-        <p className="text-sm">Thank you signing up!</p>
+      {submitted ? (
+        <p className="text-sm">Thank you for signing up!</p>
       ) : (
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="email">
-              Email Address
-            </label>
+            <label htmlFor="email">Email Address</label>
             <input 
               type="email"
               value={email}
@@ -32,12 +38,10 @@ const BookEvent = ({ eventId, slug }: { eventId: string ; slug: string }) => {
             />
           </div>
 
-          <button type="submit" className="button-submit">
-            Submit
-          </button>
+          <button type="submit" className="button-submit" >Submit</button>
         </form>
       )}
-    </div>  
+    </div>
   )
 }
 
