61b4d4c304460c63b3bef97878966ba94de03e24
dskkoji <50835363+dskkoji@users.noreply.github.com>
2026-01-19 08:24:51 +0900

    load real events

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import ExploreBtn from '@/components/ExploreBtn'
 import EventCard from '@/components/EventCard'
 import { IEvent } from '@/database'
-import events from '@/lib/constants'
 
 import { cacheLife } from 'next/cache'
 
@@ -10,8 +9,8 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 const Page = async () => {
   'use cache'
   cacheLife('hours')
-  // const response = await fetch(`${BASE_URL}/api/events`)
-  // const { events } = await response.json()
+  const response = await fetch(`${BASE_URL}/api/events`)
+  const { events } = await response.json()
 
   return (
     <section>
@@ -26,7 +25,7 @@ const Page = async () => {
         <h3>Featured Events</h3>
 
         <ul className="events">
-          {events && events.length > 0 && events.map((event) => (
+          {events && events.length > 0 && events.map((event: IEvent) => (
             <li key={event.title} className="list-none">
               <EventCard {...event} />
             </li>
