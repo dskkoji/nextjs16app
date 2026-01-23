5f4af71e49c0724c77f94fc3e9a3fc59e6fda03e
dskkoji <50835363+dskkoji@users.noreply.github.com>
2026-01-23 14:18:31 +0900

    modified

--- components/EventCreateForm.tsx
+++ components/EventCreateForm.tsx
@@ -1,12 +1,13 @@
-"use client";
-import { useState } from "react";
-import ImageUpload from "./ImageUpload";
+'use client'
+import { useState } from 'react'
+import ImageUpload from './ImageUpload'
+
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const EventCreateForm = () => {
-  const [submitted, setSubmitted] = useState(false);
+  const [submitted, setSubmitted] = useState(false)
   const initialData = {
-     title: "",
+    title: "",
     description: "",
     overview: "",
     venue: "",
@@ -18,9 +19,9 @@ const EventCreateForm = () => {
     agenda: "",
     tags: "",
     image: "" as string | File,
-    location: "",
+    location: ""
   }
-  const [formData, setFormData] = useState(initialData);
+  const [formData, setFormData] = useState(initialData)
 
   const {
     title,
@@ -36,197 +37,206 @@ const EventCreateForm = () => {
     organizer,
     image,
     location,
-  } = formData;
+  } = formData
 
-  const handleClick = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleClick = async (e: React.FormEvent) => {
     e.preventDefault()
     try {
-      const formDataToSend = new FormData();
-      formDataToSend.append("title", formData.title);
-      formDataToSend.append("description", formData.description);
-      formDataToSend.append("overview", formData.overview);
-      formDataToSend.append("venue", formData.venue);
-      formDataToSend.append("mode", formData.mode);
-      formDataToSend.append("audience", formData.audience);
-      formDataToSend.append("organizer", formData.organizer);
-      formDataToSend.append("agenda", JSON.stringify(formData.agenda));
-      formDataToSend.append("date", formData.date);
-      formDataToSend.append("time", formData.time);
-      formDataToSend.append("location", formData.location);
-      formDataToSend.append("tags", JSON.stringify(formData.tags));
+      const formDataToSend = new FormData()
+      formDataToSend.append("title", title)
+      formDataToSend.append("description", description)
+      formDataToSend.append("overview", overview)
+      formDataToSend.append("venue", venue)
+      formDataToSend.append("mode", mode)
+      formDataToSend.append("audience",audience)
+      formDataToSend.append("organizer", organizer)
+      formDataToSend.append("agenda", JSON.stringify(agenda))
+      formDataToSend.append("date", date)
+      formDataToSend.append("time", time)
+      formDataToSend.append("location", location)
+      formDataToSend.append("tags", JSON.stringify(tags))
 
-      // ファイルオブジェクトを追加
       if (formData.image instanceof File) {
-        formDataToSend.append("image", formData.image);
+        formDataToSend.append("image", image)
       }
 
       const res = await fetch(`${BASE_URL}/api/events`, {
         method: "POST",
-        body: formDataToSend,
-      });
-      setSubmitted(true);
-      setFormData(initialData)
-      return await res.json();
+        body: formDataToSend
+      })
+      
+      if (res.status !== 201) {
+        alert(`Something wrong: ${res.statusText}` )
+      } else {
+        setSubmitted(true)
+        setFormData(initialData)
+        return await res.json()
+      }
     } catch (e) {
-      console.error("Failed to create event:", e);
+      console.error("Failed to create event:", e)
     }
-  };
+  }
 
   const options = [
     { value: "online", label: "online" },
     { value: "offline", label: "offline" },
     { value: "hybrid", label: "hybrid" },
-  ];
+  ]
 
   return (
     <div id="book-event">
       <form onSubmit={handleClick}>
         <div>
-          <label htmlFor="title">Event Title</label>
-          <input
+          <label htmlFor="title">
+            Title
+          </label>
+          <input 
             type="text"
             id="title"
             name="title"
             value={title}
-            onChange={(e) =>
-              setFormData((prev) => ({ ...prev, title: e.target.value }))
-            }
-            placeholder="Enter event title"
+            placeholder="Enter Title"
+            onChange={(e) => setFormData((prev) => ({...prev, title: e.target.value }))}
           />
-          <label htmlFor="description">Description</label>
-          <textarea
-            value={description}
+          <label htmlFor="description">
+            Description
+          </label>
+          <textarea 
             id="description"
             name="description"
+            value={description}
+            placeholder="Enter Description"
+            onChange={(e) => setFormData((prev) => ({...prev, description: e.target.value }))}
             rows={8}
-            placeholder="Enter event description"
-            onChange={(e) =>
-              setFormData((prev) => ({ ...prev, description: e.target.value }))
-            }
           />
-          <label htmlFor="overview">Overview</label>
-          <input
+          <label htmlFor="overview">
+            Overview
+          </label>
+          <input 
             type="text"
-            value={overview}
             id="overview"
             name="overview"
+            value={overview}
             placeholder="Enter overview"
-            onChange={(e) =>
-              setFormData((prev) => ({ ...prev, overview: e.target.value }))
-            }
+            onChange={(e) => setFormData((prev) => ({...prev, overview: e.target.value }))}
           />
-          <label htmlFor="venue">Venue</label>
-          <input
+          <label htmlFor="venue">
+            Venue
+          </label>
+          <input 
             type="text"
-            value={venue}
             id="venue"
             name="venue"
-            onChange={(e) =>
-              setFormData((prev) => ({ ...prev, venue: e.target.value }))
-            }
+            value={venue}
             placeholder="Enter venue"
+            onChange={(e) => setFormData((prev) => ({...prev, venue: e.target.value }))}
           />
-          <label htmlFor="date">Date</label>
-          <input
+          <label htmlFor="date">
+            Date
+          </label>
+          <input 
             type="date"
             id="date"
             name="date"
             value={date}
-            onChange={(e) =>
-              setFormData((prev) => ({ ...prev, date: e.target.value }))
-            }
+            placeholder="Enter date"
+            onChange={(e) => setFormData((prev) => ({...prev, date: e.target.value }))}
           />
-          <label htmlFor="time">Time</label>
-          <input
+          <label htmlFor="time">
+            Time
+          </label>
+          <input 
             type="time"
             id="time"
             name="time"
             value={time}
-            onChange={(e) =>
-              setFormData((prev) => ({ ...prev, time: e.target.value }))
-            }
+            placeholder="Enter time"
+            onChange={(e) => setFormData((prev) => ({...prev, time: e.target.value }))}
           />
-          <label htmlFor="mode">Mode</label>
-          <select
-            name="mode"
+          <label htmlFor="mode">
+            Mode
+          </label>
+          <select 
             id="mode"
+            name="mode"
             value={mode}
-            onChange={(e) =>
-              setFormData((prev) => ({ ...prev, mode: e.target.value }))
-            }
+            onChange={(e) => setFormData((prev) => ({...prev, mode: e.target.value }))}
           >
-            {options.map((op) => (
-              <option key={op.value} value={op.value}>
-                {op.label}
-              </option>
+            {options.map((option) => (
+              <option key={option.value} value={option.value}>{option.label}</option>
             ))}
           </select>
-
-          <label htmlFor="Audience">Audience</label>
-          <input
+          <label htmlFor="audience">
+            Audience
+          </label>
+          <input 
             type="text"
             id="audience"
             name="audience"
             value={audience}
-            onChange={(e) =>
-              setFormData((prev) => ({ ...prev, audience: e.target.value }))
-            }
             placeholder="Enter audience"
+            onChange={(e) => setFormData((prev) => ({...prev, audience: e.target.value }))}
           />
-          <label htmlFor="agenda">Agenda</label>
-          <textarea
-            rows={10}
+          <label htmlFor="agenda">
+            agenda
+          </label>
+          <textarea 
             id="agenda"
             name="agenda"
             value={agenda}
-            onChange={(e) =>
-              setFormData((prev) => ({ ...prev, agenda: e.target.value }))
-            }
+            placeholder="Enter agenda"
+            onChange={(e) => setFormData((prev) => ({...prev, agenda: e.target.value }))}
+            rows={10}
           />
-          <label htmlFor="tags">Tags</label>
-          <input
+          <label htmlFor="tags">
+            Tags
+          </label>
+          <input 
+            type="text"
             id="tags"
             name="tags"
             value={tags}
-            onChange={(e) =>
-              setFormData((prev) => ({ ...prev, tags: e.target.value }))
-            }
+            placeholder="Enter tags"
+            onChange={(e) => setFormData((prev) => ({...prev, tags: e.target.value }))}
           />
-          <label>Organizer</label>
-          <input
+          <label htmlFor="organizer">
+            Organizer
+          </label>
+          <input 
+            type="text" 
             id="organizer"
             name="organizer"
             value={organizer}
-            onChange={(e) =>
-              setFormData((prev) => ({ ...prev, organizer: e.target.value }))
-            }
+            onChange={(e) => setFormData((prev) => ({ ...prev, organizer: e.target.value }))}  
             placeholder="Enter organizer"
           />
-          <label htmlFor="image">Image</label>
-          <ImageUpload
+          <label htmlFor="image">
+            Image
+          </label>
+          <ImageUpload 
             image={image}
             onImageChange={(file) =>
               setFormData((prev) => ({ ...prev, image: file }))
             }
           />
-          <label htmlFor="location">Location</label>
-          <input
-            type="text"
+          <label htmlFor="location">
+            location
+          </label>
+          <input 
+            type="text" 
             id="location"
             name="location"
             value={location}
-            onChange={(e) =>
-              setFormData((prev) => ({ ...prev, location: e.target.value }))
-            }
-            placeholder="Enter location"
+            onChange={(e) => setFormData((prev) => ({ ...prev, location: e.target.value }))}  
+            placeholder="Enter organizer"
           />
           <button type="submit" className="button-submit mt-4">
-            {submitted ? "Created Event Successfully!" : "Submit"}
+            {submitted ?  "Created Event Successfully!" : "Submit"}
           </button>
         </div>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default EventCreateForm;
+export default EventCreateForm
--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,18 +1,23 @@
-const ImageUpload = ({ image, onImageChange }: { image: any; onImageChange: (file: File) => void }) => {
+const ImageUpload = ({
+  image,
+  onImageChange,
+}: {
+  image: any;
+  onImageChange: (file: File) => void;
+}) => {
   const handleChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length !== 0) {
-      onImageChange(e.target.files[0])
+      onImageChange(e.target.files[0]);
     }
-  }
-
+  };
   return (
-    <input 
+    <input
       type="file"
       id="image"
       accept="image/*"
       onChange={handleChangeFile}
     />
-  )
-}
+  );
+};
 
-export default ImageUpload
+export default ImageUpload;
