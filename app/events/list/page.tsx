d3dafbcb7c4deb99367780dc8d903489b4d4c9f3
dskkoji <50835363+dskkoji@users.noreply.github.com>
2026-01-23 16:38:27 +0900

    fixed

--- app/events/list/page.tsx
+++ app/events/list/page.tsx
@@ -3,13 +3,13 @@ import { IEvent } from "@/database"
 import { cacheLife } from 'next/cache'
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
-import events from '@/lib/constants'
+// import events from '@/lib/constants'
 
 const EventsListPage = async () => {
   'use cache'
   cacheLife('hours')
-  // const response = await fetch(`${BASE_URL}/api/events`)
-  // const { events } = await response.json()
+  const response = await fetch(`${BASE_URL}/api/events`)
+  const { events } = await response.json()
 
   return (
     <section>
